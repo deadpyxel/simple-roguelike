a7844399ccbabf98cace9f892763d0f36ab299f4
Robson Cruz <ghost0092@gmail.com>
2020-02-08 17:40:41 -0300

    feat: Add tunnel creation do connect rooms

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -33,8 +33,10 @@ class GameMap:
         room1 = Room(x=20, y=15, w=10, h=15)
         room2 = Room(x=35, y=15, w=10, h=15)
 
+        # Hand made dungeon
         self.create_room(room1)
         self.create_room(room2)
+        self.create_h_tunnel(25, 40, 23)
 
     def create_room(self, room: Room):
         """Carve out a room in the GameMap
@@ -46,6 +48,30 @@ class GameMap:
             for y in range(room.y1 + 1, room.y2):
                 self.tiles[x][y].blocked = False
                 self.tiles[x][y].block_sight = False
+
+    def create_h_tunnel(self, x1: int, x2: int, y: int):
+        """Define a horizontal tunnel
+        
+        Arguments:
+            x1 {int} -- start of the tunnel
+            x2 {int} -- end of the tunnel
+            y {int} -- y position related to gamemap
+        """
+        for x in range(min(x1, x2), max(x1, x2) + 1):
+            self.tiles[x][y].blocked = False
+            self.tiles[x][y].block_sight = False
+
+    def create_v_tunnel(self, y1: int, y2: int, x: int):
+        """Define a vertical tunnel
+        
+        Arguments:
+            y1 {int} -- start of the tunnel
+            y2 {int} -- end of the tunnel
+            x {int} -- x position related to gamemap
+        """
+        for y in range(min(y1, y2), max(y1, y2) + 1):
+            self.tiles[x][y].blocked = False
+            self.tiles[x][y].block_sight = False
 
     def is_blocked(self, x: int, y: int) -> bool:
         """Checks if given map position is blocked
