1b7304d115c44fc1e979da2de4b0d0f87adf6c07
Robson Cruz <ghost0092@gmail.com>
2020-02-08 15:28:58 -0300

    feat: Add render functions module

---
+++ render_functions.py
@@ -0,0 +1,27 @@
+import tcod as libtcod
+
+
+def render_all(
+    con: libtcod.console, entities: list, screen_width: int, screen_height: int
+):
+    # Draw all entities in the list
+    for entity in entities:
+        draw_entity(con, entity)
+
+    libtcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
+
+
+def clear_all(con: libtcod.console, entities: list):
+    for entity in entities:
+        clear_entity(con, entity)
+
+
+def draw_entity(con: libtcod.console, entity: object):
+    libtcod.console_set_default_foreground(con, entity.color)
+    libtcod.console_put_char(con, entity.x, entity.y, entity.char, libtcod.BKGND_NONE)
+
+
+def clear_entity(con: libtcod.console, entity: object):
+    # erase the character that represents this object
+    libtcod.console_put_char(con, entity.x, entity.y, " ", libtcod.BKGND_NONE)
+
