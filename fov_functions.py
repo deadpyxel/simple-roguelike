0aeba74935fad547d87a06af18d468ca8634234e
Robson Cruz <ghost0092@gmail.com>
2020-02-08 19:16:09 -0300

    feat: Add wrapper to call FoV calculation

--- fov_functions.py
+++ fov_functions.py
@@ -9,7 +9,7 @@ def initialize_fov(game_map: object) -> libtcod.map.Map:
     
     Returns:
         libtcod.map.Map -- FoV map
-    """    
+    """
     fov_map = libtcod.map.Map(game_map.width, game_map.height)
 
     for y in range(game_map.height):
@@ -22,3 +22,26 @@ def initialize_fov(game_map: object) -> libtcod.map.Map:
                 not game_map.tiles[x][y].blocked,
             )
     return fov_map
+
+
+def recompute_fov(
+    fov_map: libtcod.map.Map,
+    x: int,
+    y: int,
+    radius: int,
+    light_walls: bool = True,
+    algorithm: int = 0,
+):
+    """Triggers FoV recaulculations using tocd
+    
+    Arguments:
+        fov_map {libtcod.map.Map} -- current FoV map
+        x {int} -- x position
+        y {int} -- y positon
+        radius {int} -- radius of view
+    
+    Keyword Arguments:
+        light_walls {bool} -- control wall lighting (default: {True})
+        algorithm {int} -- which algoritm to use (default: {0})
+    """
+    libtcod.map_compute_fov(fov_map, x, y, radius, light_walls, algorithm)
