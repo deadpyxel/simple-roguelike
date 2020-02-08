ac0f7ceade2060342655e581127d155fedf0d1b1
Robson Cruz <ghost0092@gmail.com>
2020-02-08 17:56:03 -0300

    feat: Add utility function for overlap check

--- map_objects/room.py
+++ map_objects/room.py
@@ -3,7 +3,7 @@ class Room:
     """
 
     def __init__(self, x: int, y: int, w: int, h: int):
-        """Rom initializer
+        """Room initializer
         
         Arguments:
             x {int} -- x origin
@@ -16,3 +16,28 @@ class Room:
         self.x2 = x + w
         self.y2 = y + h
 
+    def center(self) -> tuple:
+        """Returns center of room as a tuple
+        
+        Returns:
+            tuple -- center of the room
+        """
+        center_x = int((self.x1 + self.x2) / 2)
+        center_y = int((self.y1 + self.y2) / 2)
+        return (center_x, center_y)
+
+    def intersect(self, other: Room) -> bool:
+        """Checks for room intersection
+        
+        Arguments:
+            other {Room} -- other room object to comparison
+        
+        Returns:
+            bool -- True if the rooms intersect
+        """
+        return (
+            self.x1 <= other.x2
+            and self.x2 >= other.x1
+            and self.y1 <= other.y2
+            and self.y2 >= other.y1
+        )
