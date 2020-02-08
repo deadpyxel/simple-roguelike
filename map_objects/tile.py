7bb8dd4267f89a6fc4a05a413a678661995b0000
Robson Cruz <ghost0092@gmail.com>
2020-02-08 16:37:26 -0300

    feat: Add map_objects module to handle map logic

---
+++ map_objects/tile.py
@@ -0,0 +1,21 @@
+class Tile:
+    """Generic Tile class. 
+    A tile may or may not be blocked and may or may not block sight
+    """
+
+    def __init__(self, blocked: bool, block_sight: bool = None):
+        """Tile initializer.
+        
+        Arguments:
+            blocked {bool} -- blocking status of tile
+        
+        Keyword Arguments:
+            block_sight {bool} -- value determining sight status (default: {None})
+        """
+        self.blocked = blocked
+
+        # By default, if a tile is blocked, it also blocks sight
+        if block_sight is None:
+            block_sight = blocked
+
+        self.block_sight = block_sight
