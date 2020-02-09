eaf2c10b66b68fe5408efd1337f13cfc1b18324e
Robson Cruz <ghost0092@gmail.com>
2020-02-08 21:18:42 -0300

    feat: Add initial AI component for enemy

---
+++ components/ai.py
@@ -0,0 +1,8 @@
+class BasicMonster:
+    """Basic Monster behaviour component
+    """    
+    def take_turn(self):
+        """Handle turn
+        """        
+        print(f"The {self.owner.name} wonders how to move...")
+
