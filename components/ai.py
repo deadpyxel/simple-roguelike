15e39416f7b9e8f91d7eacc60af3904e92e8ba74
Robson Cruz <ghost0092@gmail.com>
2020-02-08 22:16:06 -0300

    feat: Implement enemy movement

--- components/ai.py
+++ components/ai.py
@@ -1,8 +1,20 @@
+import tcod as libtcod
+
+
 class BasicMonster:
     """Basic Monster behaviour component
-    """    
-    def take_turn(self):
+    """
+
+    def take_turn(self, target, fov_map, game_map, entities):
         """Handle turn
-        """        
-        print(f"The {self.owner.name} wonders how to move...")
+        """
+        monster = self.owner
+        # If the monster is in the FoV
+        if libtcod.map_is_in_fov(fov_map, monster.x, monster.y):
+            # If the target is too far move towards it
+            if monster.distance_to(target) >= 2:
+                monster.move_towards(target.x, target.y, game_map, entities)
+            # Else, attack
+            elif target.fighter.hp > 0:
+                print(f"The {monster.name} insults you! Your ego is damaged!")
 
--- entity.py
+++ entity.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Entity:
     """A generic class used to represent player, npcs, enemies
     """
@@ -50,6 +53,43 @@ class Entity:
         """
         self.x += dx
         self.y += dy
+
+    def move_towards(
+        self, target_x: int, target_y: int, game_map: object, entities: list
+    ):
+        """Move this entity towards a target position
+        
+        Arguments:
+            target_x {int} -- x position of target
+            target_y {int} -- y position of target
+            game_map {object} -- GameMap object
+            entities {list} -- list of entities in the map
+        """        
+        dx = target_x - self.x  # delta x
+        dy = target_y - self.y  # delta y
+        dist = math.sqrt(dx ** 2 + dy ** 2)  # Euclidian distance
+
+        dx = int(round(dx / dist))
+        dy = int(round(dy / dist))
+
+        if not (
+            game_map.is_blocked(self.x + dx, self.y + dy)
+            or get_blocking_entities_at_location(entities, self.x + dx, self.y + dy)
+        ):
+            self.move(dx, dy)
+
+    def distance_to(self, other: object) -> int or float:
+        """Returns distance between entitities
+        
+        Arguments:
+            other {Entity} -- other entity to check
+        
+        Returns:
+            int or float -- euclidian distance between entities
+        """        
+        dx = other.x - self.x
+        dy = other.y - self.y
+        return math.sqrt(dx ** 2 + dy ** 2)
 
 
 def get_blocking_entities_at_location(entities, destination_x, destination_y):
