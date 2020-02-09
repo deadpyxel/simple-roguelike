fccf717831e1abdc91e891377a4dafdfc2f9ee60
Robson Cruz <ghost0092@gmail.com>
2020-02-08 22:46:17 -0300

    feat: Replace print statements with log generation

--- components/fighter.py
+++ components/fighter.py
@@ -15,26 +15,45 @@ class Fighter:
         self.defense = defense
         self.atk_power = power
 
-    def take_damage(self, amount: int):
+    def take_damage(self, amount: int) -> list:
         """Harming behaviour
         
         Arguments:
             amount {int} -- amount of damage taken
+        
+        Returns:
+            list -- results of damage taken
         """
+        results = []
         self.hp -= amount
+        if self.hp <= 0:
+            results.append({"dead": self.owner})
+        return results
 
-    def attack(self, target:object):
+    def attack(self, target: object) -> list:
         """Attacking behaviour
         
         Arguments:
             target {object} -- target entity to receive damage
-        """        
-        damage = self.atk_power - target.fighter.defense # simple damage calculation
+        
+        Returns:
+            list -- list of results from attack action
+        """
+        results = []  # resulting log
+        damage = self.atk_power - target.fighter.defense  # simple damage calculation
 
         if damage > 0:
-            target.fighter.take_damage(damage)
-            print(f"{self.owner.name} attacks {target.name} for {damage} hit points.")
-        else:
-            print(
-                f"{self.owner.name.capitalize()} attacks {target.name} but does no damage."
+            results.append(
+                {
+                    "message": f"{self.owner.name} attacks {target.name} for {damage} hit points."
+                }
             )
+            results.extend(target.fighter.take_damage(damage))
+        else:
+            results.append(
+                {
+                    "message": f"{self.owner.name.capitalize()} attacks {target.name} but does no damage."
+                }
+            )
+
+        return results
