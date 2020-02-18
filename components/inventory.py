d76e22bfc2ededd8e894b114336bc71d9bdcfb5c
Robson Cruz <ghost0092@gmail.com>
2020-02-18 15:03:04 -0300

    feat: Add function to add item to inventory

--- components/inventory.py
+++ components/inventory.py
@@ -1,3 +1,8 @@
+import tcod as libtcod
+
+from game_messages import Message
+
+
 class Inventory:
     """Inventory class. Defines Inventory logic and behaviour
     """
@@ -11,3 +16,38 @@ class Inventory:
         self.capacity = capacity
         self.items = []  # Container that holds the items
 
+    def add_item(self, item: object) -> list:
+        """Item add behaviour. Adds an item to inventory if
+        not over capacity, else does not pick up item and
+        warns the player
+        
+        Arguments:
+            item {object} -- Item object to be picked up
+        
+        Returns:
+            list -- list of game messages resulting from player's actions
+        """
+        results = []
+
+        if len(self.items) >= self.capacity:
+            results.append(
+                {
+                    "item_added": None,
+                    "message": Message(
+                        "You cannot carry any more, your inventory is full!",
+                        color=libtcod.yellow,
+                    ),
+                }
+            )
+        else:
+            results.append(
+                {
+                    "item_added": item,
+                    "message": Message(
+                        f"You pickup a {item.name}", color=libtcod.blue,
+                    ),
+                }
+            )
+            self.items.append(item)
+
+        return results
