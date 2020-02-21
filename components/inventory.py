badf166e463e14a48803342f953f20532b5ab9ad
Robson Cruz <ghost0092@gmail.com>
2020-02-20 22:27:09 -0300

    feat: Add item usage capabilities

--- components/inventory.py
+++ components/inventory.py
@@ -51,3 +51,48 @@ class Inventory:
             self.items.append(item)
 
         return results
+
+    def remove_item(self, item: object):
+        """Remove an item from the inventory
+        
+        Arguments:
+            item {object} -- Item to be removed
+        """
+        self.items.remove(item)
+
+    def use(self, item_entity: object, **kwargs) -> list:
+        """Use an item from the inventory.
+
+        The keyword args provided will be used by the `item_use` function
+        
+        Arguments:
+            item_entity {object} -- Item to be used
+        
+        Returns:
+            list -- list of resulting actions from item usage
+        """
+        results = []
+
+        item_component = item_entity.item
+
+        # Check if the item is usable
+        if item_component.use_function is None:
+            results.append(
+                {
+                    "message": Message(
+                        f"The {item_entity.name} cannot be used", color=libtcod.yellow
+                    )
+                }
+            )
+        else:
+            kwargs = {**item_component.function_kwargs, **kwargs}
+            item_use_results = item_component.use_function(self.owner, **kwargs)
+            for result in item_use_results:
+                if result.get("consumed"):
+                    # Once item has been used, remove from inventory
+                    self.remove_item(item_entity)
+
+            results.extend(item_use_results)
+
+        return results
+
