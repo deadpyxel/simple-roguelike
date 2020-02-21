54bfd4a787e8898b3f98c844f8ef86be4595f0a4
Robson Cruz <ghost0092@gmail.com>
2020-02-20 22:10:40 -0300

    feat: Allow items to be used
    Items now can be created passing an item function as argument
    and it's kwargs.

--- components/item.py
+++ components/item.py
@@ -1,5 +1,7 @@
 class Item:
     """Item class. Defines item component behaviour
-    """    
-    def __init__(self):
-        pass
+    """
+
+    def __init__(self, use_function=None, **kwargs):
+        self.use_function = use_function
+        self.function_kwargs = kwargs
